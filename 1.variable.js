362e63da9efb3b2981d9c34f263677bdefd9f671
sunjay77-bit <sanjaylamaa77@gmail.com>
2025-12-09 14:53:28 +0545

    Basic Variables

--- 1.variable.js
+++ 1.variable.js
@@ -2,7 +2,17 @@ const accountId = 9813
 let accountemail = "jay@gmail.com"
 accountcity = "london"
 
-// accounId = 1231 not allowed
+// accounId = 1231 not allowed ( const: neither name can be changed nor the value )
+accountemail = "san@gmail.com"
+accountpassword = "982367"
+accountcity = "japan"
 
-console.log(accounId);
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in lock scope and funtional scope
+*/
+
+console.table([accountId, accountemail, accountpassword, accountcity])
 
