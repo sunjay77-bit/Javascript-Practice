fde7b7c2ad059efeb535452c9d0c4d4282ef4001
sunjay77-bit <sanjaylamaa77@gmail.com>
2025-12-13 11:48:22 +0545

    Datatype Summary

---
+++ datatypes-summary.js
@@ -0,0 +1,33 @@
+// primitive
+
+// 7 types: string, Number, Boolean, null, Undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userfamily;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+console.log(id === anotherId);
+
+const bigNumber = 123456n;
+
+// Refrence(Non-Primitive)
+
+//Array, Objects, Functions
+
+const heros = ["ironman", "batman", "spiderman"];
+
+let myObj = {
+  name: "Jay",
+  age: "23",
+};
+
+const myFunction = function () {
+    console.log("Hello World");
+}
+    console.log(typeof bigNumber);
